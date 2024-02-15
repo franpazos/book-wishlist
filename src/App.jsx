ee34a92104e813cb19c6f5eb86054396539a76fa
Fran Pazos <franciscopazosrey@gmail.com>
2024-02-15 18:08:55 +0100

    Book details page created, routes fixed, error page and home page created

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,25 @@ import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Navbar from "./components/Navbar/Navbar.jsx"
 import Homepage from "./pages/HomePage/HomePage.jsx"
+import Wishlist from "./pages/Wishlist/Wishlist.jsx"
 import BookDetails from "./pages/BookDetails/BookDetails.jsx"
+import ErrorPage from "./pages/ErrorPage/ErrorPage.jsx"
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 
 function App() {
   return (
-    <div>
-    <Navbar/>
-    <Homepage/>
-    <Routes>
-      <Route path="/" element={<Homepage/>}/>
-      <Route path="/book-details" element={<BookDetails/>}/>
-    </Routes>
+    <div className='App'>
+
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/wishlist" element={<Wishlist />} />
+        <Route path="/wishlist/:bookId" element={<BookDetails />} />
+        <Route path="*" element={<ErrorPage />} />
+
+      </Routes>
     </div>
   )
 }
