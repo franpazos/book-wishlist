61f8254f3daf9786555007076fe3180f13a4a37a
Fran Pazos <franciscopazosrey@gmail.com>
2024-02-16 13:39:40 +0100

    finished navbar, starterd DEtails

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,43 +12,41 @@ import Offcanvas from 'react-bootstrap/Offcanvas';
 
 const NavBar = () => {
 
-    const expand = 'lg'; // Choose the desired expand value
+    const expand = 'md'
 
 
     return (
-        <div className="navbar">
 
-            <Navbar expand={expand} className="bg-body-light mb-3">
-                <Container fluid>
-                    <Navbar.Brand className="img-logo" href="./">
-                        <img src={logo} alt="Web's Logo" />
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
-                    <Navbar.Offcanvas
-                        id={`offcanvasNavbar-expand-${expand}`}
-                        aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-                        placement="end"
-                    >
-                        <Offcanvas.Body>
-                            <Nav className="justify-content-end flex-grow-1 pe-3">
-                                <Nav.Link href="/">Home</Nav.Link>
-                                <Nav.Link href="/wishlist">WishList</Nav.Link>
-                            </Nav>
-                            <Form className="d-flex">
-                                <Form.Control
-                                    type="search"
-                                    placeholder="Search"
-                                    className="me-2"
-                                    aria-label="Search"
-                                />
-                                <Button variant="outline-success">Search</Button>
-                            </Form>
-                        </Offcanvas.Body>
-                    </Navbar.Offcanvas>
-                </Container>
-            </Navbar>
+        <Navbar expand={expand} className="bg-body-light mb-3">
+            <Container fluid>
+                <Navbar.Brand className="img-logo" href="./">
+                    <img src={logo} alt="Web's Logo" />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+                <Navbar.Offcanvas
+                    id={`offcanvasNavbar-expand-${expand}`}
+                    aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+                    placement="end"
+                >
+                    <Offcanvas.Body>
+                        <Nav className="justify-content-end flex-grow-1 pe-3">
+                            <Nav.Link className='links' href="/">Home</Nav.Link>
+                            <Nav.Link className='links' href="/wishlist">WishList</Nav.Link>
+                        </Nav>
+                        <Form className="d-flex">
+                            <Form.Control
+                                type="search"
+                                placeholder="Search"
+                                className="me-2"
+                                aria-label="Search"
+                            />
+                            <Button variant="outline-success">Search</Button>
+                        </Form>
+                    </Offcanvas.Body>
+                </Navbar.Offcanvas>
+            </Container>
+        </Navbar>
 
-        </div>
     )
 }
 
