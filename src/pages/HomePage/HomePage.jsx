c8af14408dc6ec3045e3c8d186ef7ed924cb88f6
Fran Pazos <franciscopazosrey@gmail.com>
2024-02-19 20:26:07 +0100

    created form and adjusted some book details and navbar

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,21 @@
+import axios from "axios"
+import { useState } from 'react'
+import { useNavigate } from "react-router-dom"
+import { Container, Button, Form, Row, Col } from "react-bootstrap"
+import NewBookForm from "./../../components/Form/NewBookForm"
+
 const HomePage = () => {
 
     return (
         <div className="homepage">
-            <h1>Hola HomePage</h1>
+            <h1>Add a new book to your Wishlist!</h1>
+
+            <Container>
+                <NewBookForm />
+            </Container>
+
+
+
         </div>
     )
 }
