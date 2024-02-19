4b519a13e04c8420a96d656402161b76fd376455
Fran Pazos <franciscopazosrey@gmail.com>
2024-02-19 01:34:15 +0100

    added toggle buttons

--- src/pages/BookDetails/BookDetails.jsx
+++ src/pages/BookDetails/BookDetails.jsx
@@ -2,7 +2,7 @@ import "./BookDetails.css"
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams, Link, useNavigate } from "react-router-dom";
-import { Container, Row, Col, Image } from "react-bootstrap";
+import { Container, Row, Col, Image, ToggleButton, ButtonGroup } from "react-bootstrap";
 
 const API_BASE_URL = "http://localhost:5005"
 
@@ -12,6 +12,8 @@ const BookDetails = () => {
     const { bookId } = useParams()
 
     const navigate = useNavigate()
+
+    const [radioValue, setRadioValue] = useState(null);
 
     useEffect(() => loadBookDetails(), [])
 
@@ -30,60 +32,95 @@ const BookDetails = () => {
             .catch(err => console.log(err))
     }
 
+    const handleStatusChange = (status) => {
+        setReadStatus(status)
+    }
+
+    const radios = [
+        { name: 'Reading', value: '1' },
+        { name: 'Read', value: '2' },
+    ]
+
     const { name, lastName } = book.author || {}
     const { publisher, year } = book.publishSpecs || {}
 
     return (
 
         <Container>
+
             <Row>
+
                 <Col xs={6} md={4}>
                     <Image src={book.cover} alt="Book Cover" thumbnail className="book-cover" />
                 </Col>
 
-                <Col>
-                    <div className="bookdetails">
+                <Col className="bookdetails">
 
-                        <h3 className="title">{book.title}</h3>
-                        <h4 className="author">Written by {name} {lastName}</h4>
-                        <p className="description">{book.description}</p>
-                        <p>Language: {book.language}</p>
-                        <p className="pages">Pages: {book.pages}</p>
-                        <p>Published by {publisher} in {year}</p>
+                    <h3 className="title">{book.title}</h3>
+                    <hr />
+                    <h4 className="author">Written by {name} {lastName}</h4>
+                    <p className="description">{book.description}</p>
+                    <p>Language: {book.language}</p>
+                    <p className="pages">Pages: {book.pages}</p>
+                    <p>Published by {publisher} in {year}</p>
 
-                        <Row>
-                            <Col>
+                    <Row>
+                        <Col>
 
-                                <div className="genres">
-                                    <strong>Genres:</strong>
-                                    <ul>
-                                        {book.genres && book.genres.map((genre, index) => (
-                                            <li key={index}>{genre}</li>
-                                        ))}
-                                    </ul>
-                                </div>
+                            <div className="genres">
+                                <strong>Genres:</strong>
+                                <ul>
+                                    {book.genres && book.genres.map((genre, index) => (
+                                        <li key={index}>{genre}</li>
+                                    ))}
+                                </ul>
+                            </div>
 
-                            </Col>
-                            <Col>
+                        </Col>
+                        <Col>
 
-                                <div className="awards">
-                                    <strong>Awards:</strong>
-                                    <ul>
-                                        {book.awards && book.awards.map((awards, index) => (
-                                            <li key={index}>{awards}</li>
-                                        ))}
-                                    </ul>
-                                </div>
+                            <div className="awards">
+                                <strong>Awards:</strong>
+                                <ul>
+                                    {book.awards && book.awards.map((awards, index) => (
+                                        <li key={index}>{awards}</li>
+                                    ))}
+                                </ul>
+                            </div>
 
-                            </Col>
-                        </Row>
+                        </Col>
 
-                    </div>
+                    </Row>
+
+                    <Row className="buttons">
+
+                        <Col>
+
+                            <ButtonGroup>
+                                {radios.map((radio, idx) => (
+                                    <ToggleButton
+                                        key={idx}
+                                        id={`radio-${idx}`}
+                                        type="radio"
+                                        variant={idx % 2 ? 'outline-success' : 'outline-secondary'}
+                                        name="radio"
+                                        value={radio.value}
+                                        checked={radioValue === radio.value}
+                                        onChange={(e) => setRadioValue(e.currentTarget.value)}
+                                    >
+                                        {radio.name}
+                                    </ToggleButton>
+                                ))}
+                            </ButtonGroup>
+
+                        </Col>
+
+                    </Row>
+
                 </Col>
+
             </Row>
-            <Row>
-                <h1>what if i put something here?</h1>
-            </Row>
+
         </Container>
     )
 }
