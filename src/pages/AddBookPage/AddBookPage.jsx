e44f22e139c5b7e09a731cf07df957d50fc56c53
Veronica Vezzoli <veronicavezzoli94@gmail.com>
2024-02-22 11:43:06 +0100

    modification style

--- src/pages/AddBookPage/AddBookPage.jsx
+++ src/pages/AddBookPage/AddBookPage.jsx
@@ -1,8 +1,10 @@
 import NewBookForm from "../../components/Forms/NewBookForm";
+import "./AddBookPage.css"
 
 const AddBookPage = () => {
     return (
-        <div className="addbookpage">
+        <div className="AddBookPage">
+            <h1>Add a New Book</h1>
             <NewBookForm/>
         </div>
     )
