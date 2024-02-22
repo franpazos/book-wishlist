f77c228a495ae240f442371fb21936ef4253a658
Veronica Vezzoli <veronicavezzoli94@gmail.com>
2024-02-22 11:26:47 +0100

    style + new book page

---
+++ src/pages/AddBookPage/AddBookPage.jsx
@@ -0,0 +1,11 @@
+import NewBookForm from "../../components/Forms/NewBookForm";
+
+const AddBookPage = () => {
+    return (
+        <div className="addbookpage">
+            <NewBookForm/>
+        </div>
+    )
+}
+
+export default AddBookPage
